30417c454ed3bd71697d42967689ae06fe4fb50c
Piyanshi101 <119510051+Piyanshi101@users.noreply.github.com>
2022-11-30 19:33:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ import streamlit as st
 st.write("multiply")
 a=st.number_input("Enter first number")
 b=st.number_input("Enter second number") 
-c=a-b
-st.write("Difference of given number is",c)
+c=a*b
+st.write("Multiplication of given number is",c)
+st.write("AGARWAL PIYANSHI SUNIL 21f1000070")
